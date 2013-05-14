6f576b96932da48676724441df1111930776467c
Helge Skogly Holm <helge.holm@gmail.com>
2013-05-14 15:40:22 +0200

    Creates and returns a sublogger tagged with the current module's name.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+var path = require('path');
 var winston = require('winston');
 var TaggedConsoleTarget = require('tagged-console-target');
 var TaggedLogger = require('tagged-logger');
+
+function moduleName() {
+  if (module && module.parent && module.parent.id) {
+    var moduleFile = path.basename(module.parent.id);
+    var nameMatch = /(.*)\.js$/.exec(moduleFile);
+    if (nameMatch && nameMatch[1])
+      return nameMatch[1];
+  }
+  return "main";
+}
 
 module.exports = function () {
   var winstonLogger = new (winston.Logger)({ transports: [ new TaggedConsoleTarget() ] });
   var log = new TaggedLogger(winstonLogger, []);
 
-  return log;
+  var tag = moduleName();
+  return log.createSublogger(tag);
 };
